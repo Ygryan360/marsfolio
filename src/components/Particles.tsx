4076a8b1a1156ab0e46dd9d06ecd754943a6c27a
Rayane Tchabodi <rayanetchabodi360@gmail.com>
2025-04-08 15:27:42 +0000

    a

--- src/components/Particles.tsx
+++ src/components/Particles.tsx
@@ -2,91 +2,96 @@
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
 import { loadSlim } from "@tsparticles/slim";
+import { Engine, ISourceOptions } from "@tsparticles/engine";
 
 
 
 const ParticlesComponent = (props: { id: string }) => {
   const [init, setInit] = useState(false);
   useEffect(() => {
-    initParticlesEngine(async (engine) => {
+    initParticlesEngine(async (engine: Engine) => {
       await loadSlim(engine);
     }).then(() => {
       setInit(true);
     });
   }, []);
 
-  const options = useMemo(
-    () =>
-    ({
-      background: {
-        color: {
-          value: "#1B1617",
+  const options: ISourceOptions = useMemo(() =>
+  ({
+    background: {
+      color: {
+        value: "#1B1617",
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "repulse",
+        },
+        onHover: {
+          enable: true,
+          mode: 'grab',
         },
       },
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "repulse",
-          },
-          onHover: {
-            enable: true,
-            mode: 'grab',
-          },
+      modes: {
+        push: {
+          distance: 200,
+          duration: 15,
         },
-        modes: {
-          push: {
-            distance: 200,
-            duration: 15,
-          },
-          grab: {
-            distance: 150,
-          },
-        },
-      },
-      particles: {
-        color: {
-          value: "#FFFFFF",
-        },
-        links: {
-          color: "#FFFFFF",
+        grab: {
           distance: 150,
-          enable: true,
-          opacity: 0.3,
-          width: 1,
-        },
-        move: {
-          direction: "none",
-          enable: true,
-          outModes: {
-            default: "bounce",
-          },
-          random: true,
-          speed: 1,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 150,
-        },
-        opacity: {
-          value: 1.0,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 3 },
         },
       },
-      detectRetina: true,
-    }),
+    },
+    particles: {
+      color: {
+        value: "#FFFFFF",
+      },
+      links: {
+        color: "#FFFFFF",
+        distance: 150,
+        enable: true,
+        opacity: 0.3,
+        width: 1,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: true,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+        },
+        value: 150,
+      },
+      opacity: {
+        value: 1.0,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 3 },
+      },
+    },
+    detectRetina: true,
+  }),
     [],
   );
 
-  return <Particles id={props.id} init={init} options={options} />;
+
+  if (init) {
+    return <Particles id={props.id} options={options} />;
+  }
+
+  return null;
 };
 export default ParticlesComponent;
